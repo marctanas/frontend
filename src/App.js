39b5a419cc511e3f96054e64d48aa89643115eb9
Marc Tanas <mstanas@hotmail.com>
2020-07-11 10:59:22 +0300

    setup loginpage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import LandingPage from './LandingPage';
+import LoginPage from './LoginPage';
+
 import './App.css';
 
 const App = () => {
@@ -8,6 +10,7 @@ const App = () => {
     <BrowserRouter>
       <Switch>
         <Route path="/" exact={true} component={LandingPage}/>
+        <Route path="/login" exact={true} component={LoginPage}/>
       </Switch>
     </BrowserRouter>
     )
---
+++ src/LoginPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavBar from './NavBar.js';
+
+
+const LoginPage = () => {
+
+    return(
+        <div>
+            <NavBar/>
+            <h1>Login</h1>
+        </div>
+    )
+
+}
+
+export default LoginPage;
